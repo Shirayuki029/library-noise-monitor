faba01d47c9fe542dac7145aa0770139e5ca60ed
Shirayuki Himeji <albanodc2006@gmail.com>
2026-06-28 23:40:28 +0800

    Update otp_verify.php

--- otp_verify.php
+++ otp_verify.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require_once 'config.php');
+require_once 'config.php'; // FIXED: Removed extra ')'
 // NO mail_config.php - we're showing OTP on screen
 
 if (isAuthenticated()) {
